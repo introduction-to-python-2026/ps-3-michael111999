1251eb208250ab2efe2496fd404d9cd7969b301a
michael111999 <micsasson7@gmail.com>
2025-11-11 20:54:18 +0200

    Update leibniz_series_exercise.py

--- leibniz_series_exercise.py
+++ leibniz_series_exercise.py
@@ -1,2 +1,9 @@
 def approximate_pi(n_terms):
-    pass # replace pass with your code
+    n_terms=10
+    lst=[]
+    for i in n_terms:
+        if i%2==0:
+            lst.append(1)
+        else:
+            lst.append(2)
+    
